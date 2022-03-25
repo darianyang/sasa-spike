86dd69c715ab7857bbef23bb07ade2f9f5c9dad9
Darian T. Yang <dty7@pitt.edu>
2022-03-25 18:29:41 -0400

    closed sasa run finished

--- vmd-sasa.sh
+++ vmd-sasa.sh
@@ -2,5 +2,5 @@
 # run sasa on spike open and closed
 
 # run spike open
-sed "s/TYPE/open" vmd-sasa.tcl > vmd-sasa-open.tcl
-vmd -dispdev text -e vmd-sasa-open.tcl
+sed "s/TYPE/open/g" vmd-sasa-tmp.tcl > vmd-sasa-open.tcl
+#vmd -dispdev text -e vmd-sasa-open.tcl
