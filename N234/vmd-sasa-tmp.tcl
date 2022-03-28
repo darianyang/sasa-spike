a53f304bc0f6a25d35f3789115a2e9cd95e1dd0a
Darian T. Yang <dty7@pitt.edu>
2022-03-28 11:47:55 -0400

    moved files around to run N709

--- N234/vmd-sasa-tmp.tcl
+++ N234/vmd-sasa-tmp.tcl
@@ -1,14 +1,14 @@
-# open new molecule and select the glycan of interest
-mol new {spike_open/spike_open.psf} type {psf} first 0 last -1 step 1 waitfor 1
-mol addfile {spike_open/spike_open.pdb} type {pdb} first 0 last -1 step 1 waitfor 1 0
+# TYPE new molecule and select the glycan of interest
+mol new {spike_TYPE/spike_TYPE.psf} type {psf} first 0 last -1 step 1 waitfor 1
+mol addfile {spike_TYPE/spike_TYPE.pdb} type {pdb} first 0 last -1 step 1 waitfor 1 0
 set G7 [atomselect top "serial 59922 to 60144"] 
 set G31 [atomselect top "serial 64727 to 64970"] 
 set G54 [atomselect top "serial 69455 to 69698"] 
 
 # selection and file header
 set protein [atomselect top "protein or lipid or glycan"]
-set output [open "SASA_open.dat" w]
-puts $output "#SASA Calculations for N234A of SARS-COV2 spike_open"
+set output [TYPE "SASA_TYPE.dat" w]
+puts $output "#SASA Calculations for N234 of SARS-COV2 spike_TYPE"
 puts $output "#probe-radius\tG7\tG31\tG54"
 
 # use the default water probe radius of 1.4A
@@ -28,7 +28,7 @@ for {set i 2} {$i < 22} {incr i} {
 }
 
 puts "Done."	
-puts "output file: SASA_open.dat"
+puts "output file: SASA_TYPE.dat"
 close $output
 
 quit
--- N234/vmd-sasa.sh
+++ N234/vmd-sasa.sh
@@ -4,3 +4,7 @@
 # run spike open
 sed "s/TYPE/open/g" vmd-sasa-tmp.tcl > vmd-sasa-open.tcl
 #vmd -dispdev text -e vmd-sasa-open.tcl
+
+# run spike closed
+sed "s/TYPE/closed/g" vmd-sasa-tmp.tcl > vmd-sasa-closed.tcl
+#vmd -dispdev text -e vmd-sasa-closed.tcl
