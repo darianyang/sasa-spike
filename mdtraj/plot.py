327d1d74571c567caea11408ffb5910f563a1986
Darian T. Yang <dty7@pitt.edu>
2022-03-29 23:37:51 -0400

    made combined plots for MDTraj sasa data

--- mdtraj/plot.py
+++ mdtraj/plot.py
@@ -6,26 +6,30 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # import datasets
-closed = np.loadtxt("SASA_closed.dat")
-opened = np.loadtxt("SASA_open.dat")
+#glycan = "N234"
+#gnames = ["G7", "G31", "G54"]
+glycan = "N709"
+gnames = ["G14", "G38", "G61"]
+closed = np.loadtxt(f"SASA_closed_{glycan}.tsv")
+opened = np.loadtxt(f"SASA_opened_{glycan}.tsv")
 
 # 2 panel figure
 fig, ax = plt.subplots(nrows=1, ncols=2, figsize=(7,4), sharex=True, sharey=True)
 
 # plot closed sasa
-ax[0].plot(closed[:,0], closed[:,1], lw=1.5, label="G14")
-ax[0].plot(closed[:,0], closed[:,2], lw=1.5, label="G38")
-ax[0].plot(closed[:,0], closed[:,3], lw=1.5, label="G61")
+ax[0].plot(closed[:,0], closed[:,1], lw=1.5, label=gnames[0])
+ax[0].plot(closed[:,0], closed[:,2], lw=1.5, label=gnames[1])
+ax[0].plot(closed[:,0], closed[:,3], lw=1.5, label=gnames[2])
 ax[0].legend()
-ax[0].set_ylabel("N709 SASA ($\AA^2$)")
+ax[0].set_ylabel(f"{glycan} SASA ($\AA^2$)")
 ax[0].set_xlabel("Probe Radius ($\AA$)")
 ax[0].set_title("Closed Spike")
 ax[0].grid(True, alpha=0.3)
 
 # plot opened sasa
-ax[1].plot(opened[:,0], opened[:,1], lw=1.5, label="G14")
-ax[1].plot(opened[:,0], opened[:,2], lw=1.5, label="G38")
-ax[1].plot(opened[:,0], opened[:,3], lw=1.5, label="G61")
+ax[1].plot(opened[:,0], opened[:,1], lw=1.5, label=gnames[0])
+ax[1].plot(opened[:,0], opened[:,2], lw=1.5, label=gnames[1])
+ax[1].plot(opened[:,0], opened[:,3], lw=1.5, label=gnames[2])
 ax[1].legend()
 ax[1].set_xlabel("Probe Radius ($\AA$)")
 ax[1].set_title("Open Spike")
@@ -34,5 +38,5 @@ ax[1].grid(True, alpha=0.3)
 ax[0].set_ylim(0,2250)
 
 fig.tight_layout()
-fig.savefig("SASA_N709.png", dpi=300, transparent=False)
+fig.savefig(f"SASA_{glycan}.png", dpi=300, transparent=False)
 plt.show()
